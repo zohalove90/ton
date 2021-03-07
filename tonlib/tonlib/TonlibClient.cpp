1e2312552ebcdba2fa544d82798cb9ec6ea03a7a
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2021-03-07 17:52:04 +0300

    Set init_block to higher value for faster sync

--- tonlib/tonlib/TonlibClient.cpp
+++ tonlib/tonlib/TonlibClient.cpp
@@ -1928,13 +1928,13 @@ const MasterConfig& get_default_master_config() {
       "root_hash": "F6OpKZKqvqeFp6CQmFomXNMfMj2EnaUSOXN+Mh+wVWk=",
       "file_hash": "XplPz01CXAps5qeSWUtxcyBfdAo5zVb1N979KLSKD24="
     },
-    "init_block" : {
-       "file_hash": "t/9VBPODF7Zdh4nsnA49dprO69nQNMqYL+zk5bCjV/8=",
-       "seqno": 8536841,
-       "root_hash": "08Kpc9XxrMKC6BF/FeNHPS3MEL1/Vi/fQU/C9ELUrkc=",
-       "workchain": -1,
-       "shard": -9223372036854775808
-   },
+   "init_block" : {
+	    "root_hash": "irEt9whDfgaYwD+8AzBlYzrMZHhrkhSVp3PU1s4DOz4=",
+	    "seqno": 10171687,
+	    "file_hash": "lay/bUKUUFDJXU9S6gx9GACQFl+uK+zX8SqHWS9oLZc=",
+	    "workchain": -1,
+	    "shard": -9223372036854775808
+    },
    "hardforks": [
       {
         "file_hash": "t/9VBPODF7Zdh4nsnA49dprO69nQNMqYL+zk5bCjV/8=",
